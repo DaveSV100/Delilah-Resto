e65bf806e543325ad4a3be003c7c2f1b000fb30a
DaveSV100 <davidsv20@icloud.com>
2022-06-09 13:00:26 -0500

    Then transformed to try and catch in users.js

--- server/users.js
+++ server/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const sequelize = require("../database/connection.js");
 const jwt = require("jsonwebtoken");
-const expressJwt = require("express-jwt");
+// const expressJwt = require("express-jwt");
 const jwtKey = process.env.JWTKEY;
 const router = express.Router();
 
@@ -11,26 +11,30 @@ const router = express.Router();
 //Loging with username and password
 // sequelize.query('INSERT INTO`restaurant`(`ID_USER`, `NOM_RESTO`, `ADRESSE`) VALUES(?, ?, ?)',
 //         { replacements: array_insert, type: sequelize.QueryTypes.SELECT }
+
 //Middleware to check if the user already exists
-const verifyUser = (req, res, next) => {
+const verifyUser = async (req, res, next) => {
     if(req.body.name && req.body.password != "") {
-        sequelize.query("SELECT * FROM usuarios WHERE name = ? && password = ?", { replacements: [req.body.name, req.body.password], type: sequelize.QueryTypes.SELECT, })
-        .then((records) => {
+        try {
+            const records = await sequelize.query("SELECT * FROM usuarios WHERE name = ? && password = ?", { replacements: [req.body.name, req.body.password], type: sequelize.QueryTypes.SELECT, })
             //This if statment verifies whether there's data or not
             if (records[0]) {
                 next();
             } else if (records[0] == null) {
                 res.status(404).send("User not found :V")
             }
-        })
+        } catch (error) {
+            console.error(error);
+        }
     } else {
         res.status(404).send("You need to insert your name and password");
     }
 }
-router.post("/", verifyUser, (req, res) => {
-  const username = req.body.name;
-  sequelize.query("SELECT * FROM usuarios WHERE name = ?", {replacements: [req.body.name],type: sequelize.QueryTypes.SELECT,})
-  .then(function (records) {
+
+router.post("/", verifyUser, async (req, res) => {
+    const username = req.body.name;
+    try {
+        await sequelize.query("SELECT * FROM usuarios WHERE name = ?", {replacements: [req.body.name],type: sequelize.QueryTypes.SELECT,})
         const token = jwt.sign({
             user: username
         }, jwtKey, { expiresIn: "1h" },);
@@ -38,20 +42,10 @@ router.post("/", verifyUser, (req, res) => {
             token
         });
         console.log(token);
-    });
-});
-
-// const verifyToken = (req, res, next) => {
-//     const bearerHeader = req.headers["authorization"];
-//     if(typeof bearerHeader !== "undefined") {
-//         const bearer = bearerHeader.split(" ");
-//         const bearerToken = bearer[1];
-//         req.token = bearerToken;
-//         next();
-//     } else {
-//         res.status(403).send("You're not a user, you need to sign up");
-//     }
-// }
+    } catch (error) {
+        console.error(error);
+    }
+  });
 
 router.get("/", (req, res) => {
     // jwt.verify(req.token, jwtKey);
